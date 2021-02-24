07dcc88fb773989aa8114764059851d8359b170e
Erik Vavro <me@madhax.io>
2021-02-23 19:34:16 -0500

    :tv: :wrench: :construction: :checkered_flag: Working v-btn-group model in controls/Settings component

--- src/components/controls/Settings.vue
+++ src/components/controls/Settings.vue
@@ -1,5 +1,6 @@
 <template>
   <v-btn-toggle
+    :value="toggle"
     dense
     group
     tile
@@ -14,6 +15,7 @@
           icon
           v-on="on"
           v-bind="attrs"
+          @click.stop="loop = !loop"
         >
           <v-icon>mdi-sync</v-icon>
         </v-btn>
@@ -30,15 +32,32 @@
           icon
           v-on="on"
           v-bind="attrs"
+          @click="follow = !follow"
         >
           <v-icon>mdi-eye-settings</v-icon>
         </v-btn>
       </template>
-      <span>Auto-scroll</span>
+      <span>Scroll with music</span>
     </v-tooltip>
   </v-btn-toggle>
 </template>
 
 <script>
+import { settings, configure } from '@/use/player'
 
+export default {
+  computed: {
+    toggle: ({ loop, follow }) => [loop, follow].map((value, index) => value ? index : null),
+
+    loop: {
+      get: () => settings.value.loop,
+      set: (value) => configure({ loop: value })
+    },
+
+    follow: {
+      get: () => settings.value.follow,
+      set: (value) => configure({ follow: value })
+    }
+  }
+}
 </script>

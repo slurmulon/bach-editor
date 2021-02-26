d15f953428aa4f0908d56a8fb2f1e3a000b15107
Erik Vavro <me@madhax.io>
2021-02-26 02:32:08 -0500

    :tv: :lipstick: :truck: :construction: Moving drawer/Tracks controls into footer for better UX

--- src/components/drawer/Tracks.vue
+++ src/components/drawer/Tracks.vue
@@ -1,78 +1,83 @@
 <template>
-  <v-list shaped>
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title class="title">
-          Tracks
-        </v-list-item-title>
-        <v-list-item-subtitle>
-          {{ count }} local tracks
-        </v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-action>
-        <create />
-      </v-list-item-action>
-    </v-list-item>
+  <div class="tracks-drawer" style="height: calc(100% - 72px); overflow: auto;">
+    <div>
+    <v-list shaped>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Tracks
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            {{ count }} local tracks
+          </v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <create />
+        </v-list-item-action>
+      </v-list-item>
 
-    <v-divider />
+      <v-divider />
 
-    <v-list-item>
-      <v-list-item-content>
-        <v-row
-          no-gutters
-          justify="center"
-        >
-          <v-spacer />
-          <!-- <v-col cols="2"> -->
-          <v-col>
-            <v-btn icon>
-              <v-icon>mdi-sort</v-icon>
-            </v-btn>
-          </v-col>
-          <v-spacer />
-          <!-- <v-col cols="2"> -->
-          <v-col>
-            <!-- <v-btn icon> -->
-            <!--   <v-icon>mdi-delete-outline</v-icon> -->
-            <!-- </v-btn> -->
-            <nuke />
-          </v-col>
-          <v-spacer />
-          <!-- <v-col cols="2"> -->
-          <v-col>
-            <archive />
-          </v-col>
-          <!-- <v-spacer /> -->
-        </v-row>
-      </v-list-item-content>
-    </v-list-item>
+      <v-list-item
+        v-for="track in all"
+        :key="track.id"
+        :input-value="active(track)"
+        @click="open(track)"
+        link
+        two-line
+      >
+        <v-list-item-content>
+          <v-list-item-title>{{ track.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ track.id }}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn
+            v-if="multiple"
+            icon
+            small
+            @click.stop="remove(track)"
+          >
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
+    </div>
 
-    <v-divider />
-
-    <v-list-item
-      v-for="track in all"
-      :key="track.id"
-      :input-value="active(track)"
-      @click="open(track)"
-      link
-      two-line
+    <v-footer
+      fixed
+      inset
+      height="72"
+      :color="!$vuetify.breakpoint.mobile ? 'rgb(12, 12, 12)' : 'grey darken-4'"
+      style="border-right: 1px solid rgba(255, 255, 255, 0.12)"
     >
-      <v-list-item-content>
-        <v-list-item-title>{{ track.name }}</v-list-item-title>
-        <v-list-item-subtitle>{{ track.id }}</v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-action>
-        <v-btn
-          v-if="multiple"
-          icon
-          small
-          @click.stop="remove(track)"
-        >
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
-      </v-list-item-action>
-    </v-list-item>
-  </v-list>
+      <v-list-item>
+        <v-list-item-content>
+          <v-row
+            no-gutters
+            justify="center"
+          >
+            <v-spacer />
+            <v-col>
+              <v-btn icon>
+                <v-icon>mdi-sort</v-icon>
+              </v-btn>
+            </v-col>
+
+            <v-spacer />
+            <v-col>
+              <nuke />
+            </v-col>
+
+            <v-spacer />
+            <v-col>
+              <archive />
+            </v-col>
+          </v-row>
+        </v-list-item-content>
+      </v-list-item>
+    </v-footer>
+  </div>
 </template>
 
 <script>
@@ -116,3 +121,8 @@ export default {
   }
 }
 </script>
+
+<style lang="sass" scoped>
+.track-drawer
+  height: calc(100% - 72px)
+  overflow: auto

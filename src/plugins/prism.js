9c7ee466a5a33c63db659f9771710d144092ceb7
Erik Vavro <me@madhax.io>
2021-05-05 16:27:04 -0400

    [v3] :tv: :lipstick: :wrench: :construction: Added most of the new syntax patterns to Prism config to match bach v3.

--- src/plugins/prism.js
+++ src/plugins/prism.js
@@ -14,7 +14,7 @@ Prism.languages.bach = {
     // alias: ['bold', 'namespace']
   },
   'constant': {
-    pattern: /(^|[^:]):\w+/,
+    pattern: /(^|[^:]):\S+/,
     lookbehind: true,
     alias: 'variable'
   },
@@ -32,16 +32,29 @@ Prism.languages.bach = {
     greedy: true
   },
   number,
-  //name,
+  // name,
   'play': {
-    pattern: /!Play/i,
+    pattern: /play!/i,
     lookahead: true
   },
   // 'builtin': /\!Play/i,
   'boolean': /\b(?:true|false)\b/,
-  'operator': /[-+%=]|\/|\*|\||!|[->]/,
+  'operator': /[-+%=]|\/|\*|\||!|[->]|of|do|when/,
   'punctuation': {
     pattern: /[{}[\](),.:]/,
     greedy: false
+  },
+  'duration': {
+    pattern: /\b(?:bar|2n|4n|8n|16n|32n|64n|128n256n)\b/,
+    greedy: false,
+    alias: 'number'
+  },
+  'match': {
+    pattern: /(?:(?:even|odd|last|first|gte|gt|lte|lt)\?)/,
+    greedy: false,
+    alias: 'operator'
+  },
+  'comment': {
+    pattern: /#{2}(.*?)(\n|\r)/
   }
 }

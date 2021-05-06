4c3a447ef13be808296a1fb800b66b20acfd8ecb
Erik Vavro <me@madhax.io>
2021-05-06 00:36:39 -0400

    [v3] :tv: :recycle: :fire: :construction: Brought back bach/validate rules logic. Need to fix bach.Music.musical (in bach-js).

--- src/bach/validate.js
+++ src/bach/validate.js
@@ -4,15 +4,15 @@ import { fail } from '@/use/notify'
 export function playable (bach) {
   const music = new Music(bach)
 
-  // for (const rule of rules) {
-  //   const valid = rule.validator(music)
+  for (const rule of rules) {
+    const valid = rule.validator(music)
 
-  //   if (!valid) {
-  //     fail({ text: rule.message, timeout: 5000 })
+    if (!valid) {
+      fail({ text: rule.message, timeout: 5000 })
 
-  //     return false
-  //   }
-  // }
+      return false
+    }
+  }
 
   return true
 }
@@ -25,16 +25,16 @@ export const rules = [
   {
     ref: 'musical',
     message: `Each beat must contain at least one musical element (${required.parts.join(', ')})`,
-    validator: bach => bach.musical
+    // FIXME (in bach-js)
+    // validator: bach => bach.musical
+    validator: bach => true
   },
   {
     ref: 'usable',
     message: 'Unsupported scale or chord type',
-    validator: sections => {
+    validator: bach => {
       try {
-        // TODO: Redo
-        // return !!sections.all
-        return true
+        return bach.elements.every(({ notes }) => !!notes.length)
       } catch (e) {
         return false
       }
